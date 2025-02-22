559b4b8b588ecf76084a97c5962fd51136b2c86a
kimberly wessler <kimberlywessler@kimberlys-MacBook-Air.local>
2025-02-22 11:11:16 -0800

    update

--- EmployeeSQL/SQL-Challenge.sql
+++ EmployeeSQL/SQL-Challenge.sql
@@ -1,22 +1,22 @@
 CREATE TABLE titles (
-    title_id Varchar NOT NULL,
-    title Varchar NOT NULL,
+    title_id Varchar(10) NOT NULL,
+    title Varchar(50) NOT NULL,
     PRIMARY KEY (title_id)
 );
 
 CREATE TABLE salaries (
-    emp_no integer Not Null,
-    salary integer Not Null,
+    emp_no Serial Not Null,
+    salary Integer Not Null,
     PRIMARY KEY (emp_no)
 )
 
 CREATE TABLE employees (
-    emp_no integer   NOT NULL,
-   	emp_title_id Varchar Not Null,
+    emp_no Serial NOT NULL,
+   	emp_title_id Varchar(10) Not Null,
 	birth_date Date Not Null,
-	first_name Varchar Not Null,
-	last_name Varchar Not Null,
-	sex Varchar Not Null,
+	first_name Varchar(50) Not Null,
+	last_name Varchar(50) Not Null,
+	sex Varchar(10) Not Null,
 	hire_date Date Not Null,
     PRIMARY KEY (emp_no),
 	FOREIGN KEY(emp_no) References Salaries (emp_no),
@@ -24,20 +24,20 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_emp (
-    emp_no integer   NOT NULL,
-   	dept_no Varchar Not Null,
-    PRIMARY KEY (emp_no),
+    emp_no Integer NOT NULL,
+   	dept_no Varchar(10) Not Null,
+    PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY(emp_no) References employees (emp_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no varchar   NOT NULL,
-   	mgr_emp_no integer Not Null,
+    dept_no varchar(10)   NOT NULL,
+   	mgr_emp_no Serial Not Null,
     PRIMARY KEY (mgr_emp_no)
 );
 
 CREATE TABLE departments (
-    dept_no varchar   NOT NULL,
-   	dept_name Varchar Not Null,
+    dept_no varchar(10)   NOT NULL,
+   	dept_name Varchar(50) Not Null,
     PRIMARY KEY (dept_no)
 );
