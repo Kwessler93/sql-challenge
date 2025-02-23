8057cf682dc24a99250b06aed20f01b08f1a47df
kimberly wessler <kimberlywessler@kimberlys-MacBook-Air.local>
2025-02-23 14:00:12 -0800

    update

--- EmployeeSQL/SQL-Challenge-Data_Analysis.sql
+++ EmployeeSQL/SQL-Challenge-Data_Analysis.sql
@@ -9,17 +9,29 @@ select first_name, last_name, hire_date
 	where extract (year from hire_date) = 1986;
 
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
-select dept_name, departments.dept_no, mgr_emp_no, last_name, first_name
-	from departments
-	join dept_manager on departments.dept_no = dept_manager.dept_no
+select mgr_emp_no, last_name, first_name, dept_manager.dept_no, dept_name
+	from dept_manager
+	join departments on departments.dept_no = dept_manager.dept_no
 	join employees on dept_manager.mgr_emp_no = employees.emp_no;
-    
+
 --List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+select employees.emp_no, last_name, first_name, dept_emp.dept_no, dept_name
+	from employees
+	join dept_emp on employees.emp_no = dept_emp.emp_no
+	join departments on dept_emp.dept_no = departments.dept_no;
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+select first_name, last_name, sex
+	from employees
+	where first_name = 'Hercules' and last_name like 'B%';
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
+select dept_emp.emp_no, last_name, first_name
+	from dept_emp
+	join employees on employees.emp_no = dept_emp.emp_no
+	where dept_no = 'd007';
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
